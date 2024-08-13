af58523257749d833bb51ef9569dcfabf7b8474c
jaymeswhyte <jmwhyte2002@gmail.com>
2024-08-13 17:51:05 +0100

    Better logging in overseerr, qbit managers and request view

--- services/qbit.py
+++ services/qbit.py
@@ -1,4 +1,5 @@
 import requests
+import logging
 
 class QBitManager:
     __username = ""
@@ -22,23 +23,29 @@ class QBitManager:
         }
         response = self.__session.post(f"{self.__address}/api/v2/auth/login", data=login_payload)
         if response.status_code==200: 
-            print(f"Connected to QBitTorrent @ {self.__address}")
+            logging.info(f"Connected to QBitTorrent @ {self.__address}")
             
-        else: print(f"Failed to connect to QBitTorrent @ {self.__address}: {response.content.decode()}")
+        else: logging.warning(f"Failed to connect to QBitTorrent @ {self.__address}: {response.content.decode()}")
         return response
 
     def pause_all(self):
-        pause_payload = {'hashes':'all'}
-        response = self.__session.post(f"{self.__address}/api/v2/torrents/pause", data=pause_payload)
-        if response.status_code != 200: 
-            print(f"Failed to pause torrents: {response.content.decode()}")
-            return False
-        else: return True
+        try:
+            pause_payload = {'hashes':'all'}
+            response = self.__session.post(f"{self.__address}/api/v2/torrents/pause", data=pause_payload)
+            if response.status_code != 200: 
+                logging.warning(f"Failed to pause torrents: {response.content.decode()}")
+                return False
+            else: return True
+        except Exception as e:
+            logging.error(f"Exception while pausing all torrents: {e}")
 
     def resume_all(self):
-        resume_payload = {'hashes':'all'}
-        response = self.__session.post(f"{self.__address}/api/v2/torrents/resume", data=resume_payload)
-        if response.status_code != 200: 
-            print(f"Failed to resume torrents: {response.content}")
-            return False
-        else: return True
+        try:
+            resume_payload = {'hashes':'all'}
+            response = self.__session.post(f"{self.__address}/api/v2/torrents/resume", data=resume_payload)
+            if response.status_code != 200: 
+                logging.warning(f"Failed to resume torrents: {response.content}")
+                return False
+            else: return True
+        except Exception as e:
+            logging.error(f"Exception while resuming all torrents: {e}")
